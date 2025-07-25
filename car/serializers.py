0b8cdfb66db11e94c6044fe8cd63af5f6619ec79
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2025-07-25 22:56:02 +0300

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    model = serializers.CharField(required=True, max_length=64)
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
+    is_broken = serializers.BooleanField(required=True)
+    problem_description = serializers.CharField(required=False, allow_null=True)
+
+    class Meta:
+        model = Car
+        fields = ('id', 'model', 'horse_powers', 'is_broken', 'problem_description')
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import json
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    serializer_data = serializer.data
+    json_output = json.dumps(serializer_data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    json_string = json.decode("utf-8")
+    data_dict = json.loads(json_string)
+    serializer = CarSerializer(data=data_dict)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
